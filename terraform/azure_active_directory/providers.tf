e92701482a216142bda82456e08ac909fb0e717f
Chad Roberts <chad.roberts@suse.com>
2024-12-10 06:43:15 -0500

    Update azure provider

--- terraform/azure_active_directory/providers.tf
+++ terraform/azure_active_directory/providers.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.56.0"
+      version = "3.74.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
